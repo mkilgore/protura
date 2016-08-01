0e3585d201a22bba10c225aaab5caa4fab0f41ba
Matt Kilgore <mattkilgore12@gmail.com>
2016-07-31 23:25:35 -0400

    Move to 16-bit dev_t to match userspace

--- include/protura/dev.h
+++ include/protura/dev.h
@@ -3,21 +3,24 @@
 
 #include <protura/types.h>
 
-#define DEV_MAJOR(dev) ((dev) >> 16)
-#define DEV_MINOR(dev) ((dev) & 0xFFFF)
+#define DEV_MAJOR(dev) ((dev) >> 8)
+#define DEV_MINOR(dev) ((dev) & 0xFF)
 
 #define DEV_MAJOR_SET(dev, major) \
     do { \
-        (dev) &= 0xFFFF; \
-        (dev) |= (major << 16); \
+        (dev) &= 0xFF; \
+        (dev) |= (major << 8); \
     } while (0)
 
 #define DEV_MINOR_SET(dev, minor) \
     do { \
-        (dev) &= 0xFFFF0000; \
+        (dev) &= 0xFF00; \
         (dev) |= (minor); \
     } while (0)
 
-#define DEV_MAKE(major, minor) (((major) << 16) + (minor))
+#define DEV_MAKE(major, minor) (((major) << 8) + (minor))
+
+#define DEV_TO_USERSPACE(dev) (((DEV_MAJOR(dev)) << 8) | (DEV_MINOR(dev)))
+#define DEV_FROM_USERSPACE(dev) DEV_MAKE(((dev) >> 8), ((dev) & 0xFF))
 
 #endif
