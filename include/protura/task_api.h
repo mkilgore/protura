f45e56adc7aae1dcb5233615507bbc3d6a5326ba
Matt Kilgore <mattkilgore12@gmail.com>
2016-09-01 09:53:41 -0400

    Add task_api.h

---
+++ include/protura/task_api.h
@@ -0,0 +1,30 @@
+#ifndef INCLUDE_PROTURA_TASK_API_H
+#define INCLUDE_PROTURA_TASK_API_H
+
+#if __KERNEL__
+# include <protura/types.h>
+# include <protura/signal.h>
+# include <protura/fs/fdset.h>
+#endif
+
+enum task_api_state {
+    TASK_API_NONE,
+    TASK_API_SLEEPING,
+    TASK_API_INTR_SLEEPING,
+    TASK_API_RUNNING,
+    TASK_API_ZOMBIE,
+};
+
+struct task_api_info {
+    pid_t pid;
+    pid_t ppid;
+    pid_t pgid;
+    enum task_api_state state;
+
+    fd_set close_on_exec;
+    sigset_t sig_pending, sig_blocked;
+
+    char name[128];
+};
+
+#endif
