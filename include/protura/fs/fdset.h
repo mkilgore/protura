3b0d0a8d7bcc8846521c8d38fcd7a1ce8ebfdaaa
Matt Kilgore <mattkilgore12@gmail.com>
2016-09-23 17:20:31 -0400

    Make fd_set use __k prefix

--- include/protura/fs/fdset.h
+++ include/protura/fs/fdset.h
@@ -8,39 +8,57 @@
 #ifndef INCLUDE_PROTURA_FS_FDSET_H
 #define INCLUDE_PROTURA_FS_FDSET_H
 
-#if __KERNEL__
+#ifdef __KERNEL__
 # include <protura/types.h>
 # include <arch/bitops.h>
 #endif
 
-#define NOFILE 32
-#define FD_SETSIZE 32
+#define __kNOFILE 32
+#define __kFD_SETSIZE 32
 
-typedef unsigned int fd_set;
-typedef unsigned int fd_mask;
+typedef unsigned int __kfd_set;
+typedef unsigned int __kfd_mask;
 
-#define NBBY 8 /* Number of bits in a byte */
+#define __kNBBY 8 /* Number of bits in a byte */
 
-#define NFDBITS (sizeof(fd_mask) * NBBY)
+#define __kNFDBITS (sizeof(fd_mask) * NBBY)
 
-static inline void FD_CLR(int fd, fd_set *set)
+static inline void __kFD_CLR(int fd, __kfd_set *set)
 {
     *set &= ~(1 << fd);
 }
 
-static inline void FD_SET(int fd, fd_set *set)
+static inline void __kFD_SET(int fd, __kfd_set *set)
 {
     *set |= (1 << fd);
 }
 
-static inline int FD_ISSET(int fd, fd_set *set)
+static inline int __kFD_ISSET(int fd, __kfd_set *set)
 {
     return !!(*set & (1 << fd));
 }
 
-static inline void FD_ZERO(fd_set *set)
+static inline void __kFD_ZERO(__kfd_set *set)
 {
     *set = 0;
 }
+
+#ifdef __KERNEL__
+
+#define NOFILE __kNOFILE
+#define FD_SETSIZE __kFD_SETSIZE
+
+typedef __kfd_set fd_set;
+typedef __kfd_mask fd_mask;
+
+#define NBBY __kNBBY
+#define FDBITS __kFDBITS
+
+#define FD_CLR(fd, set) __kFD_CLR(fd, set)
+#define FD_SET(fd, set) __kFD_SET(fd, set)
+#define FD_ISSET(fd, set) __kFD_ISSET(fd, set)
+#define FD_ZERO(set) __kFD_ZERO(set)
+
+#endif
 
 #endif
