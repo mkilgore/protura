c8fa09bed2d723b90d0b46eec22fe5c1e9563b7c
Matthew Kilgore <mattkilgore12@gmail.com>
2019-08-18 03:33:52 -0400

    Improve brk_test

--- disk/utils/brk_test.c
+++ disk/utils/brk_test.c
@@ -3,24 +3,38 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-#define INT_COUNT 128
+#define INT_COUNT 256
 
 int main(int argc, char **argv)
 {
     int i;
     int *ints;
 
-    sleep(10);
-    printf("Calling sbrk...\n");
-    ints = sbrk(INT_COUNT * sizeof(int));
+    int fd = open("/dev/ttyS1", O_RDWR);
+    dup2(fd, STDOUT_FILENO);
+    dup2(fd, STDERR_FILENO);
 
-    printf("Sleeping for 20 seconds after allocation...\n");
-    sleep(20);
+    while (1) {
+        write(STDOUT_FILENO, "Starting...\n", 12);
+        sleep(2);
+        write(STDOUT_FILENO, "Calling sbrk...\n", 16);
+        ints = malloc(INT_COUNT * sizeof(int));
 
-    for (i = 0; i < INT_COUNT; i++) {
-        ints[i] = i;
-        printf("%d\n", ints[i]);
+        printf("New malloc ptr: %p\n", ints);
+
+        printf("Sleeping for 10 seconds after allocation...\n");
+        sleep(10);
+        printf("Faulting page in...\n");
+
+        for (i = 0; i < INT_COUNT; i++) {
+            printf("Prev value: %d, ", ints[i]);
+            ints[i] = i;
+            printf("new %d\n", ints[i]);
+        }
+        printf("Writing done...\n");
+        printf("Argv 0: %s\n", argv[0]);
     }
 
     return 0;
