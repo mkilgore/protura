c28ec74fc5f2cfec0deb521cc33dba861dce15e4
Matthew Kilgore <mattkilgore12@gmail.com>
2019-08-24 15:21:06 -0400

    Add toolchain caching to speed-up most builds

--- scripts/travis_gcc.sh
+++ scripts/travis_gcc.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
-make toolchain -j3 >./toolchain.log 2>&1 &
+git submodule update --init --depth 1 --recursive ./disk/toolchain
+
+TOOL_CHAIN_HASH=$(cd ./disk/toolchain; git rev-parse HEAD:./gcc)
+CACHED_TOOLCHAIN_HASH=
+
+if [ -e ./toolchain/hash ]; then
+    CACHED_TOOLCHAIN_HASH=$(cat ./toolchain/hash)
+fi
+
+echo "GCC hash: $TOOL_CHAIN_HASH"
+echo "CACHED hash: $CACHED_TOOLCHAIN_HASH"
+
+MAKE_CMD=toolchain
+
+if [ "$TOOL_CHAIN_HASH" == "$CACHED_TOOLCHAIN_HASH" ]; then
+    echo "Using cached GCC, just building newlib..."
+    MAKE_CMD=rebuild-newlib
+fi
+
+make $MAKE_CMD -j3 >./toolchain.log 2>&1 &
 PID=$!
 
 while [ -d /proc/$PID ]
 do
-    echo "Building GCC..."
+    echo "Building Toolchain..."
     sleep 10s
 done
 
 wait $PID
 CODE=$?
 
-echo "GCC build status code: $CODE"
+echo "Toolchain build status code: $CODE"
 
 if [ $CODE -ne 0 ]; then
-    echo "GCC BUILD ERROR:"
+    echo "TOOLCHAIN BUILD ERROR:"
     cat ./toolchain.log
 fi
 
+echo $TOOL_CHAIN_HASH > ./toolchain/hash
 exit $CODE
