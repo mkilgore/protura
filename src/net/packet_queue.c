2318a0ca31645f24451139219e949a1a71ce2109
Matthew Kilgore <mattkilgore12@gmail.com>
2019-08-24 20:51:00 -0400

    packet_queue: use a workqueue instead of a kernel thread

--- src/net/packet_queue.c
+++ src/net/packet_queue.c
@@ -12,6 +12,7 @@
 #include <protura/mm/kmalloc.h>
 #include <protura/snprintf.h>
 #include <protura/list.h>
+#include <protura/work.h>
 #include <arch/asm.h>
 
 #include <protura/fs/procfs.h>
@@ -21,53 +22,22 @@
 #include <protura/net/linklayer.h>
 #include <protura/net.h>
 
-static struct task *packet_process_task;
+static struct workqueue packet_queue = WORKQUEUE_INIT(packet_queue);
 
-static spinlock_t packet_queue_lock = SPINLOCK_INIT("packet-queue-lock");
-static int packet_queue_length = 0;
-static list_head_t packet_queue = LIST_HEAD_INIT(packet_queue);
-
-static int packet_process_thread(void *p)
+static void packet_process(struct work *work)
 {
-    while (1) {
-        using_spinlock(&packet_queue_lock) {
-            kp(KP_NORMAL, "packet-queue awake\n");
+    struct packet *packet = container_of(work, struct packet, dwork.work);
 
-            while (!list_empty(&packet_queue)) {
-                struct packet *packet = list_take_first(&packet_queue, struct packet, packet_entry);
-
-                kp(KP_NORMAL, "Recieved packet! len: %d\n", packet_len(packet));
-                kp(KP_NORMAL, "Packets in queue: %d\n", --packet_queue_length);
-
-                not_using_spinlock(&packet_queue_lock)
-                    packet_linklayer_rx(packet);
-            }
-
-            sleep {
-                if (list_empty(&packet_queue)) {
-                    not_using_spinlock(&packet_queue_lock)
-                        scheduler_task_yield();
-                }
-            }
-        }
-    }
-
-    return 0;
+    kp(KP_NORMAL, "Recieved packet! len: %d\n", packet_len(packet));
+    packet_linklayer_rx(packet);
 }
 
 void net_packet_receive(struct packet *packet)
 {
-    using_spinlock(&packet_queue_lock) {
-        list_add_tail(&packet_queue, &packet->packet_entry);
-        packet_queue_length++;
-    }
+    work_init_workqueue(&packet->dwork.work, packet_process, &packet_queue);
+    work_schedule(&packet->dwork.work);
 
     kp(KP_NORMAL, "Queued packet, length: %d\n", packet_len(packet));
-
-    if (packet_process_task) {
-        kp(KP_NORMAL, "Waking packet processor\n");
-        scheduler_task_wake(packet_process_task);
-    }
 }
 
 void net_packet_transmit(struct packet *packet)
@@ -77,9 +47,6 @@ void net_packet_transmit(struct packet *packet)
 
 void net_packet_queue_init(void)
 {
-    struct task *thread = task_kernel_new_interruptable("packet-handler", packet_process_thread, NULL);
-    scheduler_task_add(thread);
-
-    atomic_ptr_swap(&packet_process_task, thread);
+    workqueue_start_multiple(&packet_queue, "packet-queue", 4);
 }
 
