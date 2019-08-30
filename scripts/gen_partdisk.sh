d3f7ef56981887d77fb037c5cde1d231ff1a961e
Matthew Kilgore <mattkilgore12@gmail.com>
2019-08-30 11:58:27 -0400

    Missing Travis dependency

--- scripts/gen_partdisk.sh
+++ scripts/gen_partdisk.sh
@@ -9,33 +9,42 @@ echo "DISK: $DISK_IMG"
 
 rm -f $DISK_IMG
 
-truncate -s 256MB $DISK_IMG
+truncate -s 256MB $DISK_IMG \
+    || exit 1
 
-sfdisk $DISK_IMG < $DIR/partition_table
+sfdisk $DISK_IMG < $DIR/partition_table \
+    || exit 1
 
 LOOP_DEVICE=$(losetup -f --show -P $DISK_IMG)
 LOOP_DEVICE_PART1=${LOOP_DEVICE}p1
 echo "Loop device: $LOOP_DEVICE"
 echo "Loop partition: $LOOP_DEVICE_PART1"
 
-mkfs.ext2 -b 4096 -O ^large_file,^dir_index,^sparse_super,^resize_inode,^filetype $LOOP_DEVICE_PART1
+mkfs.ext2 -b 4096 -O ^large_file,^dir_index,^sparse_super,^resize_inode,^filetype $LOOP_DEVICE_PART1 \
+    || exit 1
 
 mkdir -p $DISK_MOUNT
-mount $LOOP_DEVICE_PART1 $DISK_MOUNT
+mount $LOOP_DEVICE_PART1 $DISK_MOUNT \
+    || exit 1
 
-$DIR/copy_root.sh $DISK_MOUNT
+$DIR/copy_root.sh $DISK_MOUNT \
+    || exit 1
 
 if [ -x "$(command -v grub-install)" ]; then
     echo "Installing Grub to image..."
-    grub-install --target=i386-pc --boot-directory="$DISK_MOUNT/boot" $LOOP_DEVICE
+    grub-install --target=i386-pc --boot-directory="$DISK_MOUNT/boot" $LOOP_DEVICE \
+        || exit 1
 else
     echo "grub-install not found, skipping GRUB setup!"
 fi
 
-umount $DISK_MOUNT
+umount $DISK_MOUNT \
+    || exit 1
 
 echo "Unmounting loop: $LOOP_DEVICE"
-losetup -d $LOOP_DEVICE
+losetup -d $LOOP_DEVICE \
+    || exti 1
 
-chmod 666 $DISK_IMG
+chmod 666 $DISK_IMG \
+    || exti 1
 
