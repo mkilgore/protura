fdc8a01e691956eddd1e2fb3544c3f49a8eab075
Matthew Kilgore <mattkilgore12@gmail.com>
2019-09-01 13:22:17 -0400

    syscall.h: Fix syscall numbers
    It turns out there were a few duplicates here, and there were also some
    sections of numbers not used. This unifies all the numbers to ensure
    they are all unique and incremented by one from eachother.
    
    NOTE: This commit requires a full recompile of userspace to use the new
    numbers.

--- arch/x86/include/arch/syscall.h
+++ arch/x86/include/arch/syscall.h
@@ -10,106 +10,89 @@
 
 #define INT_SYSCALL 0x81
 
-#define SYSCALL_PUTCHAR 0x01
-#define SYSCALL_CLOCK   0x02
-#define SYSCALL_GETPID  0x03
-#define SYSCALL_PUTINT  0x04
-#define SYSCALL_PUTSTR  0x05
-#define SYSCALL_SLEEP   0x06
-#define SYSCALL_FORK    0x07
-#define SYSCALL_GETPPID 0x08
-#define SYSCALL_OPEN    0x09
-#define SYSCALL_CLOSE   0x0A
-#define SYSCALL_READ    0x0B
-#define SYSCALL_WRITE   0x0C
-#define SYSCALL_LSEEK   0x0D
-#define SYSCALL_EXECVE  0x0E
-#define SYSCALL_YIELD   0x0F
-#define SYSCALL_EXIT    0x10
-#define SYSCALL_WAIT    0x11
-#define SYSCALL_DUP     0x12
-#define SYSCALL_DUP2    0x13
-#define SYSCALL_BRK     0x14
-#define SYSCALL_SBRK    0x15
-#define SYSCALL_READ_DENT 0x16
-#define SYSCALL_CHDIR   0x17
-#define SYSCALL_TRUNCATE 0x18
-#define SYSCALL_FTRUNCATE 0x19
-#define SYSCALL_LINK    0x1A
-#define SYSCALL_SYNC    0x1B
-#define SYSCALL_UNLINK  0x1C
-#define SYSCALL_STAT    0x1E
-#define SYSCALL_FSTAT   0x1F
-#define SYSCALL_PIPE    0x20
-#define SYSCALL_WAITPID 0x21
-
-#define SYSCALL_SIGPROCMASK 0x22
-#define SYSCALL_SIGPENDING  0x23
-#define SYSCALL_SIGACTION   0x24
-#define SYSCALL_SIGNAL      0x25
-#define SYSCALL_KILL        0x26
-#define SYSCALL_SIGWAIT     0x27
-#define SYSCALL_SIGRETURN   0x28
-#define SYSCALL_PAUSE       0x29
-#define SYSCALL_SIGSUSPEND  0x30
-
-#define SYSCALL_MKDIR 0x31
-#define SYSCALL_MKNOD 0x32
-#define SYSCALL_RMDIR 0x33
-#define SYSCALL_RENAME 0x34
-
-#define SYSCALL_READLINK 0x35
-#define SYSCALL_SYMLINK  0x36
-#define SYSCALL_LSTAT    0x37
-
-#define SYSCALL_MOUNT 0x38
-#define SYSCALL_UMOUNT 0x39
-
-#define SYSCALL_FCNTL 0x40
-
-#define SYSCALL_TIME 0x41
-
-#define SYSCALL_SETPGID 0x42
-#define SYSCALL_GETPGRP 0x43
-#define SYSCALL_FORK_PGRP 0x44
-
-#define SYSCALL_IOCTL 0x45
-
-#define SYSCALL_POLL 0x46
-
-#define SYSCALL_SETSID 0x47
-#define SYSCALL_GETSID 0x48
-
-#define SYSCALL_SOCKET      0x49
-#define SYSCALL_SENDTO      0x4A
-#define SYSCALL_RECVFROM    0x4B
-#define SYSCALL_BIND        0x4C
-#define SYSCALL_SETSOCKOPT  0x4D
-#define SYSCALL_GETSOCKOPT  0x4E
-#define SYSCALL_GETSOCKNAME 0x4F
-#define SYSCALL_SHUTDOWN    0x50
-#define SYSCALL_SEND        0x51
-#define SYSCALL_RECV        0x52
-
-#define SYSCALL_GETTIMEOFDAY 0x60
-
-#define SYSCALL_SETUID 0x61
-#define SYSCALL_SETREUID 0x62
-#define SYSCALL_SETRESUID 0x63
-
-#define SYSCALL_GETUID 0x64
-#define SYSCALL_GETEUID 0x65
-
-#define SYSCALL_SETGID 0x66
-#define SYSCALL_SETREGID 0x67
-#define SYSCALL_SETRESGID 0x68
-
-#define SYSCALL_GETGID 0x69
-#define SYSCALL_GETEGID 0x6A
-
-#define SYSCALL_SETGROUPS 0x6B
-#define SYSCALL_GETGROUPS 0x69
-#define SYSCALL_UNAME 0x6A
+#define SYSCALL_PUTCHAR      0x01
+#define SYSCALL_CLOCK        0x02
+#define SYSCALL_GETPID       0x03
+#define SYSCALL_PUTINT       0x04
+#define SYSCALL_PUTSTR       0x05
+#define SYSCALL_SLEEP        0x06
+#define SYSCALL_FORK         0x07
+#define SYSCALL_GETPPID      0x08
+#define SYSCALL_OPEN         0x09
+#define SYSCALL_CLOSE        0x0A
+#define SYSCALL_READ         0x0B
+#define SYSCALL_WRITE        0x0C
+#define SYSCALL_LSEEK        0x0D
+#define SYSCALL_EXECVE       0x0E
+#define SYSCALL_YIELD        0x0F
+#define SYSCALL_EXIT         0x10
+#define SYSCALL_WAIT         0x11
+#define SYSCALL_DUP          0x12
+#define SYSCALL_DUP2         0x13
+#define SYSCALL_BRK          0x14
+#define SYSCALL_SBRK         0x15
+#define SYSCALL_READ_DENT    0x16
+#define SYSCALL_CHDIR        0x17
+#define SYSCALL_TRUNCATE     0x18
+#define SYSCALL_FTRUNCATE    0x19
+#define SYSCALL_LINK         0x1A
+#define SYSCALL_SYNC         0x1B
+#define SYSCALL_UNLINK       0x1C
+#define SYSCALL_STAT         0x1D
+#define SYSCALL_FSTAT        0x1E
+#define SYSCALL_PIPE         0x1F
+#define SYSCALL_WAITPID      0x20
+#define SYSCALL_SIGPROCMASK  0x21
+#define SYSCALL_SIGPENDING   0x22
+#define SYSCALL_SIGACTION    0x23
+#define SYSCALL_SIGNAL       0x24
+#define SYSCALL_KILL         0x25
+#define SYSCALL_SIGWAIT      0x26
+#define SYSCALL_SIGRETURN    0x27
+#define SYSCALL_PAUSE        0x28
+#define SYSCALL_SIGSUSPEND   0x29
+#define SYSCALL_MKDIR        0x2A
+#define SYSCALL_MKNOD        0x2B
+#define SYSCALL_RMDIR        0x2C
+#define SYSCALL_RENAME       0x2D
+#define SYSCALL_READLINK     0x2E
+#define SYSCALL_SYMLINK      0x2F
+#define SYSCALL_LSTAT        0x30
+#define SYSCALL_MOUNT        0x31
+#define SYSCALL_UMOUNT       0x32
+#define SYSCALL_FCNTL        0x33
+#define SYSCALL_TIME         0x34
+#define SYSCALL_SETPGID      0x35
+#define SYSCALL_GETPGRP      0x36
+#define SYSCALL_FORK_PGRP    0x37
+#define SYSCALL_IOCTL        0x38
+#define SYSCALL_POLL         0x39
+#define SYSCALL_SETSID       0x3A
+#define SYSCALL_GETSID       0x3B
+#define SYSCALL_SOCKET       0x3C
+#define SYSCALL_SENDTO       0x3D
+#define SYSCALL_RECVFROM     0x3E
+#define SYSCALL_BIND         0x3F
+#define SYSCALL_SETSOCKOPT   0x40
+#define SYSCALL_GETSOCKOPT   0x41
+#define SYSCALL_GETSOCKNAME  0x42
+#define SYSCALL_SHUTDOWN     0x43
+#define SYSCALL_SEND         0x44
+#define SYSCALL_RECV         0x45
+#define SYSCALL_GETTIMEOFDAY 0x46
+#define SYSCALL_SETUID       0x47
+#define SYSCALL_SETREUID     0x48
+#define SYSCALL_SETRESUID    0x49
+#define SYSCALL_GETUID       0x4A
+#define SYSCALL_GETEUID      0x4B
+#define SYSCALL_SETGID       0x4C
+#define SYSCALL_SETREGID     0x4D
+#define SYSCALL_SETRESGID    0x4E
+#define SYSCALL_GETGID       0x4F
+#define SYSCALL_GETEGID      0x50
+#define SYSCALL_SETGROUPS    0x51
+#define SYSCALL_GETGROUPS    0x52
+#define SYSCALL_UNAME        0x53
 
 #ifdef __KERNEL__
 #ifndef ASM
