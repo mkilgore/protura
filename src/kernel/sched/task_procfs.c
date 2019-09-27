c57d62409e3b1e35de353f94297ab91562d8d23b
Matthew Kilgore <mattkilgore12@gmail.com>
2019-09-26 22:54:29 -0400

    tasks: convert to use seq_list api

--- src/kernel/sched/task_procfs.c
+++ src/kernel/sched/task_procfs.c
@@ -20,39 +20,18 @@
 #include <protura/scheduler.h>
 #include "scheduler_internal.h"
 
-struct task_seq_state {
-    struct task *current_task;
-};
-
 static int task_seq_start(struct seq_file *seq)
 {
-    struct task_seq_state *state = kzalloc(sizeof(*state), PAL_KERNEL);
-    if (!state)
-        return -ENOMEM;
-
-    seq->priv = state;
     spinlock_acquire(&ktasks.lock);
 
-    if (seq->iter_offset == 0)
-        return 0;
-
-    state->current_task = list_first_entry(&ktasks.list, struct task, task_list_node);
-
-    int i;
-    for (i = 1; i < seq->iter_offset; i++)
-        state->current_task = list_next_entry(state->current_task, task_list_node);
-
-    return 0;
+    return seq_list_start_header(seq, &ktasks.list);
 }
 
 static int task_seq_render(struct seq_file *seq)
 {
-    struct task_seq_state *state = seq->priv;
-
-    if (!state->current_task)
+    struct task *t = seq_list_get_entry(seq, struct task, task_list_node);
+    if (!t)
         return seq_printf(seq, "Pid\tPPid\tPGid\tState\tKilled\tName\n");
-
-    struct task *t = state->current_task;
 
     return seq_printf(seq, "%d\t%d\t%d\t%s\t%d\t\"%s\"\n",
             t->pid,
@@ -65,28 +44,12 @@ static int task_seq_render(struct seq_file *seq)
 
 static int task_seq_next(struct seq_file *seq)
 {
-    struct task_seq_state *state = seq->priv;
-
-    if (!state->current_task)
-        state->current_task = list_first_entry(&ktasks.list, struct task, task_list_node);
-    else
-        state->current_task = list_next_entry(state->current_task, task_list_node);
-
-    seq->iter_offset++;
-
-    if (list_ptr_is_head(&ktasks.list, &state->current_task->task_list_node)) {
-        kp(KP_NORMAL, "Task rendering done!\n");
-        flag_set(&seq->flags, SEQ_FILE_DONE);
-    }
-
-    return 0;
+    return seq_list_next(seq, &ktasks.list);
 }
 
 static void task_seq_end(struct seq_file *seq)
 {
-    struct task_seq_state *state = seq->priv;
     spinlock_release(&ktasks.lock);
-    kfree(state);
 }
 
 const static struct seq_file_ops task_seq_file_ops = {
