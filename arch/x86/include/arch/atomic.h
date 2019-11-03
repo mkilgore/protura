72ba1059fb2cf7854047956058ec6a6ef55b5156
Matthew Kilgore <mattkilgore12@gmail.com>
2019-11-02 21:54:12 -0400

    atomic: add *_return() functions
    These new functions return the new value of the atomic_t after
    doing the manipulation.

--- arch/x86/include/arch/atomic.h
+++ arch/x86/include/arch/atomic.h
@@ -35,11 +35,28 @@ static __always_inline void atomic32_add(atomic32_t *v, int32_t i)
             : "ir" (i));
 }
 
+static __always_inline int32_t atomic32_xadd(atomic32_t *v, int32_t i)
+{
+    asm volatile(LOCK_PREFIX "xaddl %1, %0"
+            : "+m" (v->counter), "+r" (i));
+    return i;
+}
+
+static __always_inline int32_t atomic32_add_return(atomic32_t *v, int32_t i)
+{
+    return i + atomic32_xadd(v, i);
+}
+
 static __always_inline void atomic32_sub(atomic32_t *v, int32_t i)
 {
     asm volatile(LOCK_PREFIX "subl %1, %0"
             : "+m" (v->counter)
             : "ir" (i));
+}
+
+static __always_inline int32_t atomic32_sub_return(atomic32_t *v, int32_t i)
+{
+    return atomic32_xadd(v, -i) - i;
 }
 
 static __always_inline void atomic32_inc(atomic32_t *v)
@@ -48,10 +65,20 @@ static __always_inline void atomic32_inc(atomic32_t *v)
             : "+m" (v->counter));
 }
 
+static __always_inline int32_t atomic32_inc_return(atomic32_t *v)
+{
+    return atomic32_add_return(v, 1);
+}
+
 static __always_inline void atomic32_dec(atomic32_t *v)
 {
     asm volatile(LOCK_PREFIX "decl %0"
             : "+m" (v->counter));
+}
+
+static __always_inline int32_t atomic32_dec_return(atomic32_t *v)
+{
+    return atomic32_add_return(v, -1);
 }
 
 static __always_inline int atomic32_dec_and_test(atomic32_t *v)
@@ -76,10 +103,19 @@ typedef atomic32_t atomic_t;
 
 #define atomic_get(a)          atomic32_get(a)
 #define atomic_set(a, i)       atomic32_set(a, i)
+
 #define atomic_add(a, i)       atomic32_add(a, i)
+#define atomic_add_return(a, i) atomic32_add_return(a, i)
+
 #define atomic_sub(a, i)       atomic32_sub(a, i)
+#define atomic_sub_return(a, i) atomic32_sub_return(a, i)
+
 #define atomic_inc(a)          atomic32_inc(a)
+#define atomic_inc_return(a)   atomic32_inc_return(a)
+
 #define atomic_dec(a)          atomic32_dec(a)
+#define atomic_dec_return(a)   atomic32_dec_return(a)
+
 #define atomic_dec_and_test(a) atomic32_dec_and_test(a)
 
 #define ATOMIC_INIT(x) ATOMIC32_INIT(x)
