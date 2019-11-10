ec2f5c7ba57bfb1175802ed5d7749de06659a44f
Matthew Kilgore <mattkilgore12@gmail.com>
2019-11-10 00:19:34 -0500

    e1000: completely rewrite driver to work
    This is an almost complete rewrite of the E1000 driver. With these
    changes, it works with both QEMU and VirtualBox.

--- src/drivers/net/e1000/e1000.c
+++ src/drivers/net/e1000/e1000.c
@@ -15,13 +15,19 @@
 #include <protura/string.h>
 #include <protura/mm/kmalloc.h>
 #include <protura/mm/kmmap.h>
+#include <protura/list.h>
 #include <protura/snprintf.h>
 #include <arch/asm.h>
+#include <arch/irq.h>
+#include <arch/idt.h>
+#include <arch/drivers/pic8259.h>
 
 #include <protura/fs/procfs.h>
 #include <protura/drivers/pci.h>
 #include <protura/drivers/pci_ids.h>
+#include <protura/net/netdevice.h>
 #include <protura/net/arp.h>
+#include <protura/net/arphrd.h>
 #include <protura/net.h>
 #include <protura/drivers/e1000.h>
 #include "e1000_internal.h"
@@ -65,6 +71,7 @@ static void e1000_eeprom_detect(struct net_interface_e1000 *e1000)
     }
 }
 
+/* NOTE: EEPROM addresses count 16-bit words, not 8-bit bytes */
 static uint16_t e1000_eeprom_read(struct net_interface_e1000 *e1000, uint8_t addr)
 {
     uint32_t result = 0;
@@ -86,29 +93,206 @@ static void e1000_read_mac(struct net_interface_e1000 *e1000)
     if (e1000->has_eeprom) {
         uint16_t tmp;
         tmp = e1000_eeprom_read(e1000, 0);
-        e1000->mac[0] = tmp & 0xFF;
-        e1000->mac[1] = tmp >> 8;
+        e1000->net.mac[0] = tmp & 0xFF;
+        e1000->net.mac[1] = tmp >> 8;
+
+        tmp = e1000_eeprom_read(e1000, 1);
+        e1000->net.mac[2] = tmp & 0xFF;
+        e1000->net.mac[3] = tmp >> 8;
 
         tmp = e1000_eeprom_read(e1000, 2);
-        e1000->mac[2] = tmp & 0xFF;
-        e1000->mac[3] = tmp >> 8;
-
-        tmp = e1000_eeprom_read(e1000, 4);
-        e1000->mac[4] = tmp & 0xFF;
-        e1000->mac[5] = tmp >> 8;
+        e1000->net.mac[4] = tmp & 0xFF;
+        e1000->net.mac[5] = tmp >> 8;
 
     } else {
         int i;
         for (i = 0; i < 6; i++) {
-            e1000->mac[i] = ((uint8_t *)e1000->mem_base)[0x5400 + i];
+            e1000->net.mac[i] = ((uint8_t *)e1000->mem_base)[0x5400 + i];
         }
     }
 
-    kp(KP_ERROR, "E1000 MAC: "PRmac"\n", Pmac(e1000->mac));
+    e1000->net.hwtype = ARPHRD_ETHER;
+}
+
+static void __e1000_rx_interrupt(struct net_interface_e1000 *e1000)
+{
+    int got_packet = 0;
+    int old_rx = 0;
+
+    while (e1000->rx_descs[e1000->cur_rx].status & RXD_STAT_DD) {
+        struct e1000_rx_desc *desc = e1000->rx_descs + e1000->cur_rx;
+
+        void *buf = P2V(desc->addr);
+        kp(KP_NORMAL, "Got Rx packet!\n");
+
+        if (desc->length < PG_SIZE) {
+            struct packet *packet = packet_new(PAL_KERNEL | PAL_ATOMIC);
+
+            memcpy(packet->start, buf, desc->length);
+            packet->head = packet->start;
+            packet->tail = packet->head + desc->length;
+
+            packet->iface_rx = netdev_dup(&e1000->net);
+            net_packet_receive(packet);
+        } else {
+            kp(KP_WARNING, "E1000: packet length too long: %d, dropped.\n", desc->length);
+        }
+
+        got_packet = 1;
+        old_rx = e1000->cur_rx;
+        e1000->cur_rx = (e1000->cur_rx + 1) % E1000_NUM_RX_DESC;
+    }
+
+    /* The RDT 'lags' the cur_rx by one */
+    if (got_packet)
+        e1000_command_write32(e1000, REG_RDT, old_rx);
+}
+
+static void __e1000_clear_tx_descs(struct net_interface_e1000 *e1000)
+{
+    struct e1000_tx_desc *descs = e1000->tx_descs;
+
+    kp(KP_NORMAL, "e1000: clearing tx descs, last_clear: %d\n", e1000->last_clear);
+    int next = (e1000->last_clear + 1) % E1000_NUM_TX_DESC;
+    if (next == e1000->next_tx)
+        return;
+
+    /* Any packets with TSTA_DD set has already been sent by the hardware */
+    while (descs[next].status & TSTA_DD) {
+        kp(KP_NORMAL, "e1000: cleared desc %d\n", next);
+        descs[next].status = 0;
+        pa_t page = PG_ALIGN_DOWN(descs[next].addr);
+
+        pfree_pa(page, 0);
+
+        e1000->last_clear = next;
+        next = (next + 1) % E1000_NUM_TX_DESC;
+    }
+}
+
+static int __e1000_tx_queue_has_space(struct net_interface_e1000 *e1000)
+{
+    return e1000->next_tx != e1000->last_clear;
+}
+
+static void __e1000_queue_tx(struct net_interface_e1000 *e1000, struct packet *packet)
+{
+    struct e1000_tx_desc *desc = e1000->tx_descs + e1000->next_tx;
+
+    kp(KP_NORMAL, "e1000: queuing tx packet!\n");
+    void *head = packet->head;
+    size_t len = packet_len(packet);
+
+    /* We steal the page from the packet so we don't need to do a copy. The
+     * 'head' value we save above is inside of this page. */
+    packet_take_page(packet);
+
+    desc->cmd = TXD_CMD_RS | TXD_CMD_EOP | TXD_CMD_IFCS;
+    desc->length = len;
+    desc->addr = V2P(head);
+
+    e1000->next_tx = (e1000->next_tx + 1) % E1000_NUM_TX_DESC;
+    e1000_command_write32(e1000, REG_TDT, e1000->next_tx);
+
+    packet_free(packet);
+}
+
+static void __e1000_flush_packet_queue(struct net_interface_e1000 *e1000)
+{
+    while (__e1000_tx_queue_has_space(e1000) && !list_empty(&e1000->tx_packet_queue)) {
+        struct packet *packet = list_take_first(&e1000->tx_packet_queue, struct packet, packet_entry);
+
+        __e1000_queue_tx(e1000, packet);
+    }
 }
 
 static void e1000_packet_send(struct net_interface *net, struct packet *packet)
 {
+    struct net_interface_e1000 *e1000 = container_of(net, struct net_interface_e1000, net);
+
+    kp(KP_NORMAL, "e1000: sending packet!\n");
+
+    using_spinlock(&e1000->tx_lock) {
+        __e1000_clear_tx_descs(e1000);
+        __e1000_flush_packet_queue(e1000);
+
+        /* If we still have space after flushing the queue then send packet
+         * directly. If not, then queue it for later */
+        if (__e1000_tx_queue_has_space(e1000))
+            __e1000_queue_tx(e1000, packet);
+        else {
+            kp(KP_NORMAL, "e1000: queued packet for sending later...\n");
+            list_add_tail(&e1000->tx_packet_queue, &packet->packet_entry);
+        }
+    }
+}
+
+static void e1000_interrupt(struct irq_frame *frame, void *param)
+{
+    struct net_interface_e1000 *e1000 = param;
+
+    uint32_t icr = e1000_command_read32(e1000, REG_ICR);
+    e1000_command_write32(e1000, REG_ICR, icr);
+
+    kp(KP_NORMAL, "INT E1000! ICR: 0x%08x, status: %08x\n", icr, e1000_command_read32(e1000, REG_STATUS));
+    if (icr & (ICR_RXT0 | ICR_LSC)) {
+        using_spinlock(&e1000->rx_lock)
+            __e1000_rx_interrupt(e1000);
+    }
+
+    /* We turn on the TXDW interrupt, but we check and try to queue packets
+     * even if it isn't a TXDW */
+    using_spinlock(&e1000->tx_lock) {
+        __e1000_clear_tx_descs(e1000);
+        __e1000_flush_packet_queue(e1000);
+    }
+}
+
+static void e1000_setup_rx(struct net_interface_e1000 *e1000)
+{
+    e1000->rx_descs = kzalloc(sizeof(*e1000->rx_descs) * E1000_NUM_RX_DESC, PAL_KERNEL);
+    e1000->cur_rx = 0;
+
+    int i;
+    for (i = 0; i < E1000_NUM_RX_DESC; i++) {
+        /* Each recieve descriptor gets a single page */
+        e1000->rx_descs[i].addr = palloc_pa(0, PAL_KERNEL);
+        e1000->rx_descs[i].status = 0;
+    }
+
+    e1000_command_write32(e1000, REG_RDBAH, 0);
+    e1000_command_write32(e1000, REG_RDBAL, V2P(e1000->rx_descs));
+
+    e1000_command_write32(e1000, REG_RDLEN, E1000_NUM_RX_DESC * sizeof(*e1000->rx_descs));
+
+    e1000_command_write32(e1000, REG_RDH, 0);
+    e1000_command_write32(e1000, REG_RDT, E1000_NUM_RX_DESC - 1);
+
+    e1000_command_write32(e1000, REG_RCTRL, RCTL_EN
+                                          | RCTL_BAM
+                                          | RCTL_SECRC
+                                          | RCTL_BSIZE_4096
+                                          );
+}
+
+static void e1000_setup_tx(struct net_interface_e1000 *e1000)
+{
+    e1000->tx_descs = kzalloc(sizeof(*e1000->tx_descs) * E1000_NUM_TX_DESC, PAL_KERNEL);
+    e1000->last_clear = E1000_NUM_TX_DESC - 1;
+    e1000->next_tx = 0;
+
+    e1000_command_write32(e1000, REG_TDBAH, 0);
+    e1000_command_write32(e1000, REG_TDBAL, V2P(e1000->tx_descs));
+
+    e1000_command_write32(e1000, REG_TDLEN, E1000_NUM_TX_DESC * sizeof(*e1000->tx_descs));
+
+    e1000_command_write32(e1000, REG_TDH, 0);
+    e1000_command_write32(e1000, REG_TDT, 0);
+
+    e1000_command_write32(e1000, REG_TCTRL, TCTL_EN
+                                          | TCTL_PSP
+                                          | (0x10 << TCTL_CT_SHIFT)
+                                          );
 }
 
 void e1000_device_init(struct pci_dev *dev)
@@ -117,6 +301,9 @@ void e1000_device_init(struct pci_dev *dev)
     uint16_t command_reg;
 
     net_interface_init(&e1000->net);
+    spinlock_init(&e1000->rx_lock, "e1000-rx-lock");
+    spinlock_init(&e1000->tx_lock, "e1000-tx-lock");
+    list_head_init(&e1000->tx_packet_queue);
 
     kp(KP_NORMAL, "Found Intel E1000 NIC: "PRpci_dev"\n", Ppci_dev(dev));
 
@@ -136,9 +323,8 @@ void e1000_device_init(struct pci_dev *dev)
         uint32_t mem = pci_config_read_uint32(dev, PCI_REG_BAR(0)) & 0xFFFFFFF0;
         uint32_t size = pci_bar_size(dev, PCI_REG_BAR(0));
 
-        e1000->mem_base = kmmap(mem, size, F(VM_MAP_READ) | F(VM_MAP_WRITE) | F(VM_MAP_NOCACHE) | F(VM_MAP_WRITETHROUGH));
-        kp(KP_NORMAL, "  E1000 MEM: 0x%08x\n", mem);
-        kp(KP_NORMAL, "  E1000 MEM size: %d\n", size);
+        e1000->mem_base = kmmap(mem, size, F(VM_MAP_READ) | F(VM_MAP_WRITE) | F(VM_MAP_NOCACHE));
+        kp(KP_NORMAL, "  E1000 MEM: 0x%08x, %d bytes\n", mem, size);
     }
 
     kp(KP_NORMAL, "  E1000 IO base: 0x%04x\n", e1000->io_base);
@@ -146,6 +332,25 @@ void e1000_device_init(struct pci_dev *dev)
 
     e1000_eeprom_detect(e1000);
     e1000_read_mac(e1000);
+
+    kp(KP_NORMAL, "  E1000 MAC: "PRmac"\n", Pmac(e1000->net.mac));
+
+    int int_line = pci_config_read_uint8(dev, PCI_REG_INTERRUPT_LINE);
+    kp(KP_NORMAL, "  Interrupt: %d\n", int_line);
+
+    irq_register_callback(PIC8259_IRQ0 + int_line, e1000_interrupt, "E1000", IRQ_INTERRUPT, e1000);
+
+    e1000_setup_rx(e1000);
+    e1000_setup_tx(e1000);
+
+    e1000_command_write32(e1000, REG_IMS, IMS_RXT0
+                                        | IMS_TXDW
+                                        | IMS_RXDMT0
+                                        | IMS_RXSEQ
+                                        | IMS_LSC
+                                        );
+
+    e1000_command_write32(e1000, REG_CTRL, ECTRL_SLU | ECTRL_FD);
 
     net_interface_register(&e1000->net);
 }
--- src/drivers/net/e1000/e1000_internal.h
+++ src/drivers/net/e1000/e1000_internal.h
@@ -5,24 +5,25 @@
 #include <protura/drivers/pci.h>
 #include <protura/net.h>
 
-#define REG_CTRL        0x0000
-#define REG_STATUS      0x0008
-#define REG_EEPROM      0x0014
-#define REG_CTRL_EXT    0x0018
-#define REG_IMASK       0x00D0
-#define REG_RCTRL       0x0100
-#define REG_RXDESCLO    0x2800
-#define REG_RXDESCHI    0x2804
-#define REG_RXDESCLEN   0x2808
-#define REG_RXDESCHEAD  0x2810
-#define REG_RXDESCTAIL  0x2818
+#define REG_CTRL     0x0000
+#define REG_STATUS   0x0008
+#define REG_EEPROM   0x0014
+#define REG_CTRL_EXT 0x0018
+#define REG_ICR      0x00C0
+#define REG_IMS      0x00D0
+#define REG_RCTRL    0x0100
+#define REG_RDBAL    0x2800
+#define REG_RDBAH    0x2804
+#define REG_RDLEN    0x2808
+#define REG_RDH      0x2810
+#define REG_RDT      0x2818
 
-#define REG_TCTRL       0x0400
-#define REG_TXDESCLO    0x3800
-#define REG_TXDESCHI    0x3804
-#define REG_TXDESCLEN   0x3808
-#define REG_TXDESCHEAD  0x3810
-#define REG_TXDESCTAIL  0x3818
+#define REG_TCTRL 0x0400
+#define REG_TDBAL 0x3800
+#define REG_TDBAH 0x3804
+#define REG_TDLEN 0x3808
+#define REG_TDH   0x3810
+#define REG_TDT   0x3818
 
 #define REG_RDTR        0x2820 /* RX Delay Timer Register */
 #define REG_RXDCTL      0x3828 /* RX Descriptor Control */
@@ -31,7 +32,7 @@
 
 #define REG_TIPG        0x0410      /* Transmit Inter Packet Gap */
 #define ECTRL_SLU       0x40        /* set link up */
-
+#define ECTRL_FD        0x00000001  /* Full duplex.0=half; 1=full */
 
 #define RCTL_EN            (1 << 1)    /* Receiver Enable */
 #define RCTL_SBP           (1 << 2)    /* Store Bad Packets */
@@ -64,20 +65,17 @@
 #define RCTL_BSIZE_8192  ((2 << 16) | (1 << 25))
 #define RCTL_BSIZE_16384 ((1 << 16) | (1 << 25))
 
-
 /* Transmit Command */
-
-#define CMD_EOP  (1 << 0)    /* End of Packet */
-#define CMD_IFCS (1 << 1)    /* Insert FCS */
-#define CMD_IC   (1 << 2)    /* Insert Checksum */
-#define CMD_RS   (1 << 3)    /* Report Status */
-#define CMD_RPS  (1 << 4)    /* Report Packet Sent */
-#define CMD_VLE  (1 << 6)    /* VLAN Packet Enable */
-#define CMD_IDE  (1 << 7)    /* Interrupt Delay Enable */
+#define TXD_CMD_EOP  (1 << 0)    /* End of Packet */
+#define TXD_CMD_IFCS (1 << 1)    /* Insert FCS */
+#define TXD_CMD_IC   (1 << 2)    /* Insert Checksum */
+#define TXD_CMD_RS   (1 << 3)    /* Report Status */
+#define TXD_CMD_RPS  (1 << 4)    /* Report Packet Sent */
+#define TXD_CMD_VLE  (1 << 6)    /* VLAN Packet Enable */
+#define TXD_CMD_IDE  (1 << 7)    /* Interrupt Delay Enable */
 
 
 /* TCTL Register */
-
 #define TCTL_EN         (1 << 1)    /* Transmit Enable */
 #define TCTL_PSP        (1 << 3)    /* Pad Short Packets */
 #define TCTL_CT_SHIFT   4           /* Collision Threshold */
@@ -90,19 +88,72 @@
 #define TSTA_LC (1 << 2)    /* Late Collision */
 #define LSTA_TU (1 << 3)    /* Transmit Underrun */
 
+/* IMS flags */
+#define IMS_TXDW   0x00000001 /* Transmit desc written back */
+#define IMS_LSC    0x00000004 /* Link Status Change */
+#define IMS_RXSEQ  0x00000008 /* rx sequence error */
+#define IMS_RXDMT0 0x00000010 /* rx desc min. threshold */
+#define IMS_RXT0   0x00000080 /* rx timer intr */
 
-struct net_interface_e1000 {
-    struct net_interface net;
+/* ICR flags */
+#define ICR_TXDW          0x00000001	/* Transmit desc written back */
+#define ICR_TXQE          0x00000002	/* Transmit Queue empty */
+#define ICR_LSC           0x00000004	/* Link Status Change */
+#define ICR_RXSEQ         0x00000008	/* rx sequence error */
+#define ICR_RXDMT0        0x00000010	/* rx desc min. threshold (0) */
+#define ICR_RXO           0x00000040	/* rx overrun */
+#define ICR_RXT0          0x00000080	/* rx timer intr (ring 0) */
+#define ICR_MDAC          0x00000200	/* MDIO access complete */
+#define ICR_RXCFG         0x00000400	/* RX /c/ ordered set */
+#define ICR_GPI_EN0       0x00000800	/* GP Int 0 */
+#define ICR_GPI_EN1       0x00001000	/* GP Int 1 */
+#define ICR_GPI_EN2       0x00002000	/* GP Int 2 */
+#define ICR_GPI_EN3       0x00004000	/* GP Int 3 */
+#define ICR_TXD_LOW       0x00008000
+#define ICR_SRPD          0x00010000
+#define ICR_ACK           0x00020000	/* Receive Ack frame */
+#define ICR_MNG           0x00040000	/* Manageability event */
+#define ICR_DOCK          0x00080000	/* Dock/Undock */
+#define ICR_INT_ASSERTED  0x80000000	/* If this bit asserted, the driver should claim the interrupt */
+#define ICR_RXD_FIFO_PAR0 0x00100000	/* queue 0 Rx descriptor FIFO parity error */
+#define ICR_TXD_FIFO_PAR0 0x00200000	/* queue 0 Tx descriptor FIFO parity error */
+#define ICR_HOST_ARB_PAR  0x00400000	/* host arb read buffer parity error */
+#define ICR_PB_PAR        0x00800000	/* packet buffer parity error */
+#define ICR_RXD_FIFO_PAR1 0x01000000	/* queue 1 Rx descriptor FIFO parity error */
+#define ICR_TXD_FIFO_PAR1 0x02000000	/* queue 1 Tx descriptor FIFO parity error */
+#define ICR_ALL_PARITY    0x03F00000	/* all parity error bits */
+#define ICR_DSW           0x00000020	/* FW changed the status of DISSW bit in the FWSM */
+#define ICR_PHYINT        0x00001000	/* LAN connected device generates an interrupt */
+#define ICR_EPRST         0x00100000	/* ME hardware reset occurs */
 
-    io_t io_base;
-    void *mem_base;
-    char mac[6];
-    struct task *recv_helper;
-    struct pci_dev dev;
+/* RX desc status flags */
+#define RXD_STAT_DD    0x01   /* Descriptor Done */
+#define RXD_STAT_EOP   0x02   /* End of Packet */
+#define RXD_STAT_IXSM  0x04   /* Ignore checksum */
+#define RXD_STAT_VP    0x08   /* IEEE VLAN Packet */
+#define RXD_STAT_UDPCS 0x10   /* UDP xsum calculated */
+#define RXD_STAT_TCPCS 0x20   /* TCP xsum calculated */
+#define RXD_STAT_IPCS  0x40   /* IP xsum calculated */
+#define RXD_STAT_PIF   0x80   /* passed in-exact filter */
+#define RXD_STAT_IPIDV 0x200  /* IP identification valid */
+#define RXD_STAT_UDPV  0x400  /* Valid UDP checksum */
+#define RXD_STAT_ACK   0x8000 /* ACK Packet indication */
 
-    uint8_t has_eeprom :1;
-};
+/* RX desc err flags */
+#define RXD_ERR_CE   0x01 /* CRC Error */
+#define RXD_ERR_SE   0x02 /* Symbol Error */
+#define RXD_ERR_SEQ  0x04 /* Sequence Error */
+#define RXD_ERR_CXE  0x10 /* Carrier Extension Error */
+#define RXD_ERR_TCPE 0x20 /* TCP/UDP Checksum Error */
+#define RXD_ERR_IPE  0x40 /* IP Checksum Error */
+#define RXD_ERR_RXE  0x80 /* Rx Data Error */
 
+/* RX desc special flags */
+#define RXD_SPC_VLAN_MASK 0x0FFF /* VLAN ID is in lower 12 bits */
+#define RXD_SPC_PRI_MASK  0xE000 /* Priority is in upper 3 bits */
+#define RXD_SPC_PRI_SHIFT 13
+#define RXD_SPC_CFI_MASK  0x1000 /* CFI is bit 12 */
+#define RXD_SPC_CFI_SHIFT 12
 
 #define E1000_NUM_RX_DESC 32
 #define E1000_NUM_TX_DESC 8
@@ -125,5 +176,25 @@ struct e1000_tx_desc {
     volatile uint8_t css;
     volatile uint16_t special;
 } __packed;
+
+struct net_interface_e1000 {
+    struct net_interface net;
+
+    io_t io_base;
+    void *mem_base;
+    struct pci_dev dev;
+
+    uint8_t has_eeprom :1;
+
+    spinlock_t rx_lock;
+    struct e1000_rx_desc *rx_descs;
+    int cur_rx; /* RDT = cur_rx - 1 */
+
+    spinlock_t tx_lock;
+    list_head_t tx_packet_queue;
+    struct e1000_tx_desc *tx_descs;
+    int last_clear; /* TDH - but updated manually */
+    int next_tx; /* TDT = next_tx */
+};
 
 #endif
