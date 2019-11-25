38c7fb4703159b4d278e3758af320d0a7d0635e3
Matthew Kilgore <mattkilgore12@gmail.com>
2019-11-25 00:14:54 -0500

    semaphore: change to use sleep_event_spinlock()

--- src/kernel/sched/semaphore.c
+++ src/kernel/sched/semaphore.c
@@ -12,6 +12,16 @@
 #include <protura/semaphore.h>
 #include <protura/scheduler.h>
 
+static void __sem_wake_if_waiting(struct semaphore *sem)
+{
+    struct semaphore_wait_entry *ent;
+
+    if (!list_empty(&sem->queue)) {
+        ent = list_first_entry(&sem->queue, struct semaphore_wait_entry, next);
+        scheduler_task_wake(ent->task);
+    }
+}
+
 static void __sem_down(struct semaphore *sem)
 {
     struct semaphore_wait_entry ent;
@@ -20,20 +30,30 @@ static void __sem_down(struct semaphore *sem)
     sem_wait_entry_init(&ent);
     ent.task = cpu_get_local()->current;
 
-  sleep_again:
-    if (!list_node_is_in_list(&ent.next))
-        list_add_tail(&sem->queue, &ent.next);
+    /*
+     * The logic here is actually fairly straight forward:
+     *
+     * 1. You add yourself at the end of the queue of tasks waiting on this semaphore
+     * 2. You wait until sem->count > 0 (sleeping and releasing sem->lock if necessary)
+     * 3. You remove yourself from the list and decrement sem->count
+     * 4. We wake the next task if the semaphore still has space.
+     *
+     * The 4th step is probably the most interesting. sem_up never modifies the
+     * sem->queue, so if sem_up is done twice before the first sleeping task
+     * wakes up, then that task receives two wake-ups. This we may exit the
+     * sleeping with sem->count > 1. When that happens we have to make sure we
+     * wake up the next waiter (if there is one) to prevent a stall where
+     * waiters wouldn't be woken-up immediately.
+     */
+    list_add_tail(&sem->queue, &ent.next);
 
-    sleep {
-        if (sem->count <= 0) {
-            not_using_spinlock(&sem->lock)
-                scheduler_task_yield();
+    sleep_event_spinlock(sem->count > 0, &sem->lock);
 
-            goto sleep_again;
-        }
-    }
-    sem->count--;
     list_del(&ent.next);
+    sem->count--;
+
+    if (sem->count > 0)
+        __sem_wake_if_waiting(sem);
 
     kp(KP_LOCK, "semaphore %p down result: %d\n", sem, sem->count);
 }
@@ -57,17 +77,8 @@ static int __sem_try_down(struct semaphore *sem)
 
 static void __sem_up(struct semaphore *sem)
 {
-    struct semaphore_wait_entry *ent;
-    int wake = 0;
     sem->count++;
-
-    if (!list_empty(&sem->queue)) {
-        ent = list_take_first(&sem->queue, struct semaphore_wait_entry, next);
-        scheduler_task_wake(ent->task);
-        wake = 1;
-    }
-
-    kp(KP_LOCK, "semaphore %p: up: %d, %d\n", sem, sem->count, wake);
+    __sem_wake_if_waiting(sem);
 }
 
 void sem_down(struct semaphore *sem)
