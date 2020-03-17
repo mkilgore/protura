2fd214300fc9f444359a223972bcd6f0433313e0
Matthew Kilgore <mattkilgore12@gmail.com>
2020-03-17 19:14:16 -0400

    uapi: convert syscall.h

--- include/uapi/protura/syscall.h
+++ include/uapi/protura/syscall.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (C) 2015 Matt Kilgore
+ * Copyright (C) 2020 Matt Kilgore
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License v2 as published by the
  * Free Software Foundation.
  */
-#ifndef INCLUDE_PROTURA_SYSCALL_H
-#define INCLUDE_PROTURA_SYSCALL_H
+#ifndef __INCLUDE_UAPI_PROTURA_SYSCALL_H__
+#define __INCLUDE_UAPI_PROTURA_SYSCALL_H__
 
-#include <uapi/protura/syscall.h>
+#include <arch/syscall.h>
 
 #endif
