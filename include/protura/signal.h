bfbba0a95cfb435ad4d0f2b17e90e53daeef72b8
Matthew Kilgore <mattkilgore12@gmail.com>
2020-03-17 19:18:25 -0400

    uapi: convert signal.h

--- include/protura/signal.h
+++ include/protura/signal.h
@@ -1,9 +1,14 @@
+/*
+ * Copyright (C) 2020 Matt Kilgore
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ */
 #ifndef INCLUDE_PROTURA_SIGNAL_H
 #define INCLUDE_PROTURA_SIGNAL_H
 
-#include <arch/signal.h>
-
-#ifdef __KERNEL__
+#include <uapi/protura/signal.h>
 
 int sys_sigprocmask(int how, struct user_buffer set, struct user_buffer oldset);
 int sys_sigpending(struct user_buffer set);
@@ -16,7 +21,5 @@ int sys_sigsuspend(const struct user_buffer mask);
 
 #define has_pending_signal(task) \
     ((task)->sig_pending & ~((task)->sig_blocked))
-
-#endif
 
 #endif
---
+++ include/uapi/protura/signal.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2020 Matt Kilgore
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ */
+#ifndef __INCLUDE_UAPI_PROTURA_SIGNAL_H__
+#define __INCLUDE_UAPI_PROTURA_SIGNAL_H__
+
+#include <arch/signal.h>
+
+#endif
