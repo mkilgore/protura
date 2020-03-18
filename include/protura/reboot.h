cc3b4d60e3b5e8aecc8ab0f631772c65a1405d7f
Matthew Kilgore <mattkilgore12@gmail.com>
2020-03-17 23:31:06 -0400

    uapi: convert reboot.h

--- include/protura/reboot.h
+++ include/protura/reboot.h
@@ -1,13 +1,8 @@
 #ifndef INCLUDE_PROTURA_REBOOT_H
 #define INCLUDE_PROTURA_REBOOT_H
 
-#define PROTURA_REBOOT_MAGIC1 0xABCDBEEF
-#define PROTURA_REBOOT_MAGIC2 152182804
+#include <uapi/protura/reboot.h>
 
-#define PROTURA_REBOOT_RESTART 0x12341234
-
-#ifdef __KERNEL__
 int sys_reboot(int magic1, int magic2, int cmd);
-#endif
 
 #endif
---
+++ include/uapi/protura/reboot.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020 Matt Kilgore
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ */
+#ifndef __INCLUDE_UAPI_PROTURA_REBOOT_H__
+#define __INCLUDE_UAPI_PROTURA_REBOOT_H__
+
+#define PROTURA_REBOOT_MAGIC1 0xABCDBEEF
+#define PROTURA_REBOOT_MAGIC2 152182804
+
+#define PROTURA_REBOOT_RESTART 0x12341234
+
+#endif
