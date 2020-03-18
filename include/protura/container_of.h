b674873e44367d983dfc0bc85ad8aede15f4cdd5
Matthew Kilgore <mattkilgore12@gmail.com>
2020-03-17 23:15:13 -0400

    uapi: convert container_of.h

--- include/protura/container_of.h
+++ include/protura/container_of.h
@@ -8,10 +8,6 @@
 #ifndef INCLUDE_PROTURA_CONTAINER_OF_H
 #define INCLUDE_PROTURA_CONTAINER_OF_H
 
-#ifndef container_of
-# define container_of(ptr, type, member) ({ \
-         const typeof(((type *)0)->member) *__mptr = (ptr); \
-         (type *)((char *)__mptr - offsetof(type, member)); })
-#endif
+#include <uapi/protura/container_of.h>
 
 #endif
