d17eef20dae33b23566938c86b71fd8f7e614bf7
Matthew Kilgore <mattkilgore12@gmail.com>
2020-03-17 20:54:29 -0400

    uapi: convert fs/ioctl.h

--- include/protura/fs/ioctl.h
+++ include/protura/fs/ioctl.h
@@ -8,11 +8,6 @@
 #ifndef INCLUDE_PROTURA_FS_IOCTL_H
 #define INCLUDE_PROTURA_FS_IOCTL_H
 
-#define FIONCLEX 0x5450
-#define FIOCLEX  0x5451
-#define FIONREAD 0x541B
-
-#define FIBMAP 0x0001
-#define FIGETBSZ 0x0002
+#include <uapi/protura/fs/ioctl.h>
 
 #endif
---
+++ include/uapi/protura/fs/ioctl.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020 Matt Kilgore
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License v2 as published by the
+ * Free Software Foundation.
+ */
+#ifndef __INCLUDE_UAPI_PROTURA_FS_IOCTL_H__
+#define __INCLUDE_UAPI_PROTURA_FS_IOCTL_H__
+
+#define FIONCLEX 0x5450
+#define FIOCLEX  0x5451
+#define FIONREAD 0x541B
+
+#define FIBMAP 0x0001
+#define FIGETBSZ 0x0002
+
+#endif
