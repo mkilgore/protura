838474eb5e5215e590a2abc3ed37b4fe7df3f55a
Matthew Kilgore <mattkilgore12@gmail.com>
2020-03-17 22:41:11 -0400

    uapi: convert net/arphrd.h

--- include/protura/net/arphrd.h
+++ include/protura/net/arphrd.h
@@ -1,19 +1,6 @@
 #ifndef INCLUDE_PROTURA_NET_ARPHRD_H
 #define INCLUDE_PROTURA_NET_ARPHRD_H
 
-#define ETH_P_ARP 0x0806
-#define ETH_P_IP  0x0800
-
-#define ARPHRD_ETHER 1
-#define ARPHRD_LOOPBACK 772
-
-#define	ARPOP_REQUEST 1
-#define	ARPOP_REPLY   2
-
-struct sockaddr_ether {
-    unsigned short sa_family;
-    char sa_mac[6]; /* Mac address */
-    char sa_data[8];  /* Padding */
-};
+#include <uapi/protura/net/arphrd.h>
 
 #endif
