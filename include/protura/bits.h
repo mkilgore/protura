69a4a02e7dc437f4587a7b9eab547443bd737eef
Matthew Kilgore <mattkilgore12@gmail.com>
2020-06-17 21:36:14 -0400

    flags: allow passing multiple flags to F()
    The multiple values are or'd together into one value

--- include/protura/bits.h
+++ include/protura/bits.h
@@ -8,6 +8,7 @@
 #ifndef INCLUDE_PROTURA_BITS
 #define INCLUDE_PROTURA_BITS
 
+#include <protura/stddef.h>
 #include <protura/limits.h>
 #include <arch/bitops.h>
 
@@ -26,6 +27,33 @@
 #define b32(binhigh, binmid1, binmid2, binlow) (((unsigned int)b8(binhigh) << 24) + (b8(binmid1) << 16) + (b8(binmid2) << 8) + b8(binlow))
 
 /* Sets the bit referred to by 'flag'. */
-#define F(flag) (1 << (flag))
+#define __F1(flag) (1 << (flag))
+#define __F2(...)  (__F1(CAR(__VA_ARGS__)) | __F1(CDR(__VA_ARGS__)))
+#define __F3(...)  (__F1(CAR(__VA_ARGS__)) | __F2(CDR(__VA_ARGS__)))
+#define __F4(...)  (__F1(CAR(__VA_ARGS__)) | __F3(CDR(__VA_ARGS__)))
+#define __F5(...)  (__F1(CAR(__VA_ARGS__)) | __F4(CDR(__VA_ARGS__)))
+#define __F6(...)  (__F1(CAR(__VA_ARGS__)) | __F5(CDR(__VA_ARGS__)))
+#define __F7(...)  (__F1(CAR(__VA_ARGS__)) | __F6(CDR(__VA_ARGS__)))
+#define __F8(...)  (__F1(CAR(__VA_ARGS__)) | __F7(CDR(__VA_ARGS__)))
+#define __F9(...)  (__F1(CAR(__VA_ARGS__)) | __F8(CDR(__VA_ARGS__)))
+#define __F10(...) (__F1(CAR(__VA_ARGS__)) | __F9(CDR(__VA_ARGS__)))
+#define __F11(...) (__F1(CAR(__VA_ARGS__)) | __F10(CDR(__VA_ARGS__)))
+#define __F12(...) (__F1(CAR(__VA_ARGS__)) | __F11(CDR(__VA_ARGS__)))
+#define __F13(...) (__F1(CAR(__VA_ARGS__)) | __F12(CDR(__VA_ARGS__)))
+#define __F14(...) (__F1(CAR(__VA_ARGS__)) | __F13(CDR(__VA_ARGS__)))
+#define __F15(...) (__F1(CAR(__VA_ARGS__)) | __F14(CDR(__VA_ARGS__)))
+#define __F16(...) (__F1(CAR(__VA_ARGS__)) | __F15(CDR(__VA_ARGS__)))
+#define __F17(...) (__F1(CAR(__VA_ARGS__)) | __F16(CDR(__VA_ARGS__)))
+#define __F18(...) (__F1(CAR(__VA_ARGS__)) | __F17(CDR(__VA_ARGS__)))
+#define __F19(...) (__F1(CAR(__VA_ARGS__)) | __F18(CDR(__VA_ARGS__)))
+#define __F20(...) (__F1(CAR(__VA_ARGS__)) | __F19(CDR(__VA_ARGS__)))
+
+#define __FINNER2(cnt) \
+    __F ## cnt
+
+#define __FINNER(cnt) \
+    __FINNER2(cnt)
+
+#define F(...) (__FINNER(COUNT_ARGS(__VA_ARGS__)) (__VA_ARGS__))
 
 #endif
