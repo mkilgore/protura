b7e2ce8ecf6d85e3932e372e854cfc2ee42e01d6
Matthew Kilgore <mattkilgore12@gmail.com>
2020-09-23 22:20:10 -0400

    coreutils: dump_mem: 127 is not a renderable ASCII char
    We were currently rendering all characters from 32 to 127, but 127
    itself is not actually renderable (It's the DEL marker).

--- userspace/coreutils/common/dump_mem.c
+++ userspace/coreutils/common/dump_mem.c
@@ -27,7 +27,7 @@ void dump_mem(const void *buf, size_t len, uint32_t base_addr)
 
         cur_b += sprintf(cur_b, " |");
         for (j = i; j < i + 16 && j < len; j++)
-            if (b[j] > 31 && b[j] <= 127)
+            if (b[j] > 31 && b[j] < 127)
                 cur_b += sprintf(cur_b, "%c", b[j]);
             else
                 *(cur_b++) = '.';
